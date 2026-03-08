15c5e1a89201e467b9a6ed129e7b58b7e4072ea7
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-08 11:30:50 -0400

    Added testers and casinos models

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, \
+    String, Float, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import declarative_base
 
 # Define the base class for our models
@@ -9,4 +10,25 @@ class Quants(Base):
     
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+
+class Testers(Base):
+    __tablename__ = 'testers'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    assigned_quant = Column(Integer, ForiegnKey='quants.id')
+    created_at = Column(DateTime, nullable=False)
+
+class Casinos(Base):
+    __tablename__ = 'casinos'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    network = Column(String(50), nullable=True)
+    signup_rest = Column(Boolean, nullable=False) # True if the casino has a signup restriction, False otherwise
+    deposit_rest = Column(Boolean, nullable=False) # True if the casino has a deposit restriction, False otherwise
+    play_rest = Column(Boolean, nullable=False)
+    withdrawal_rest = Column(Boolean, nullable=False)
+    network_rest = Column(Boolean, nullable=False)
     created_at = Column(DateTime, nullable=False)
