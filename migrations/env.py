77a08c0a0abdeb96b7429e927d4efdf5d16f7362
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-08 12:33:29 -0400

    S*%! fix for env var issue

--- migrations/env.py
+++ migrations/env.py
@@ -10,11 +10,11 @@ import os
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-db_user = os.getenv("POSTGRES_USER")
-db_pass = os.getenv("POSTGRES_PASSWORD")
-db_host = os.getenv("POSTGRES_HOST")
-db_port = os.getenv("POSTGRES_PORT")
-db_name = os.getenv("POSTGRES_DB")
+db_user = os.getenv("POSTGRES_USER", "sunbird")
+db_pass = os.getenv("POSTGRES_PASSWORD", "sunbird")
+db_host = os.getenv("POSTGRES_HOST", "localhost")
+db_port = os.getenv("POSTGRES_PORT", "5432")
+db_name = os.getenv("POSTGRES_DB", "sunbird_db")
 
 database_url = f"postgresql+psycopg2://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
 
