541d13bad1b3c29d2f8de1bd7bd15d20f7522d7c
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-08 16:54:26 -0400

    session helper

---
+++ app/db/db.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.db.models import Base
+
+# Database connection URL
+DATABASE_URL = 'postgresql+psychopg2://sunbird:sunbird@localhost:5432/sunbird_db'
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+
+# Create a session factory
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+# Function to create the tables
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+# Dependency to get a session
+def get_session():
+    return SessionLocal()
