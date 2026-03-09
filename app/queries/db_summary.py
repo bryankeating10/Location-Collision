65d4a97db3b9e16bac178d460f3f638de13339b8
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-08 21:15:08 -0400

    First query test
    Successful!!

---
+++ app/queries/db_summary.py
@@ -0,0 +1,42 @@
+from sqlalchemy import text
+from app.db.db import get_session
+
+def get_summary():
+    sesh = get_session()
+
+    quant_count = sesh.execute(
+        text('SELECT COUNT(*) FROM quants')
+    )
+
+    tester1_name = sesh.execute(
+        text('SELECT name FROM testers WHERE id = 1')
+    )
+
+    casino1_name = sesh.execute(
+        text('SELECT name FROM casinos WHERE id = 1')
+    )
+
+    long1 = sesh.execute(
+        text('SELECT longitude FROM locations WHERE id = 1')
+    )
+
+    lat1 = sesh.execute(
+        text('SELECT latitude FROM locations WHERE id = 1')
+    )
+
+    result = (
+        quant_count.fetchone()[0],
+        tester1_name.fetchone()[0],
+        casino1_name.fetchone()[0],
+        long1.fetchone()[0],
+        lat1.fetchone()[0],
+    )
+
+    sesh.close()
+    return result
+
+
+summary = get_summary()
+
+print(f'There are {summary[0]} quants, the first tester is named {summary[1]}, playing on {summary[2]} casino.')
+print(f'The coordinates are {summary[3]}, {summary[4]}')
