c97bc1045aa97fea7efcc05f8383020b7e41ea43
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-08 21:00:23 -0400

    Added long/latitude coordinates to location entry

--- app/scripts/demo_insert.py
+++ app/scripts/demo_insert.py
@@ -15,7 +15,7 @@ casino1 = create_casino("DraftKings", "DK")
 account1 = create_account(tester_id=tester1.id, casino_id=casino1.id, username='moses3242')
 
 # Add location
-location1 = create_location(name='Starbucks', address='123 Main St, Butler, NJ')
+location1 = create_location(name='Starbucks', address='123 Main St, Butler, NJ', longitude=-75.3210242893, latitude=40.8530201337)
 
 # Add action
 action1 = create_action(category='signup', account_id=account1.id, location_id=location1.id)
