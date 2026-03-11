45498c8d488ea55fa1a2f672bd158d2867da00d8
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-11 10:53:56 -0400

    Accounts table populated

--- app/scripts/demo-data/create_accounts.py
+++ app/scripts/demo-data/create_accounts.py
@@ -1,22 +1,21 @@
-from sqlalchemy import text
+from sqlalchemy import select
 from random import randint
 
 from app.db.db import get_session
 from app.db.crud import create_account
-
+from app.db.models import Testers, Casinos
 
 def gen_username(name):
     return f"{name}_{randint(1000,9999)}"
 
-
 def main(testers, casinos):
     for tester in testers:
         for casino in casinos:
-            username = gen_username(tester["name"])
+            username = gen_username(tester.name)
 
             create_account(
-                tester_id=tester["id"],
-                casino_id=casino["id"],
+                tester_id=tester.id,
+                casino_id=casino.id,
                 username=username
             )
 
@@ -24,8 +23,8 @@ def main(testers, casinos):
 if __name__ == "__main__":
     sesh = get_session()
 
-    testers = list(sesh.execute(text("SELECT id, name FROM testers")).mappings())
-    casinos = list(sesh.execute(text("SELECT id FROM casinos")).mappings())
+    testers = sesh.scalars(select(Testers)).all()
+    casinos = sesh.scalars(select(Casinos)).all()
 
     main(testers, casinos)
 
